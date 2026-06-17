1995bdbba493484f0b390301d2e9ae55bcc8d06b
Wanjiku Kariuki <wanjiku7101@gmail.com>
2026-06-17 13:49:42 +0300

    Update app to display pricing dataframe

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello Cocktail Pricing App")
+import streamlit as st
+import pandas as pd
+
+st.title("Cocktail Pricing Engine")
+
+df = pd.read_csv("data/cocktail_final_prices.csv")
+
+st.dataframe(df)
